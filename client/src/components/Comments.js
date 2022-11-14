3624ca95c870b79be5ade06688907a206951be5f
Weider Lima <weider.lima@mercadolivre.com>
2022-11-14 16:42:07 -0300

    adjust comments components

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,19 +1,33 @@
-import React, { useState } from "react";
-// import socketIO from "socket.io-client";
+import React, { useEffect, useState } from "react";
+import socketIO from "socket.io-client";
+import { useParams } from "react-router-dom";
 
-// const socket = socketIO.connect("http://localhost:4000");
+const socket = socketIO.connect("http://localhost:4000");
 
 const Comments = () => {
+    const { category, id } = useParams();
     const [comment, setComment] = useState("");
+    const [commentList, setCommentList] = useState([]);
 
     const addComment = (e) => {
         e.preventDefault();
-        console.log({
+
+        socket.emit("addComment", {
             comment,
+            category,
+            id,
             userId: localStorage.getItem("userId"),
         });
         setComment("");
     };
+
+    useEffect(() => {
+        socket.on("comments", (data) => setCommentList(data));
+    }, []);
+
+    useEffect(() => {
+        socket.emit("fetchComments", { category, id });
+    }, [category, id]);
 
     return (
         <div className='comments__container'>
@@ -30,10 +44,16 @@ const Comments = () => {
                 ></textarea>
                 <button className='commentBtn'>ADD COMMENT</button>
             </form>
-
             <div className='comments__section'>
                 <h2>Existing Comments</h2>
-                <div></div>
+                {commentList.map((comment) => (
+                    <div key={comment.id}>
+                        <p>
+                            <span style={{ fontWeight: "bold" }}>{comment.text} </span>by{" "}
+                            {comment.name}
+                        </p>
+                    </div>
+                ))}
             </div>
         </div>
     );
