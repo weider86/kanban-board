021e208a7b0936e969731c17b6093890119a235e
Weider Lima <weider.lima@mercadolivre.com>
2022-11-14 16:15:28 -0300

    add task scope initial components

---
+++ client/src/components/Task.js
@@ -0,0 +1,23 @@
+import React from "react";
+import AddTask from "./AddTask";
+import TasksContainer from "./TasksContainer";
+import Nav from "./Nav";
+import socketIO from "socket.io-client";
+
+/*
+👇🏻  Pass Socket.io into the required components
+    where communications are made with the server
+*/
+const socket = socketIO.connect("http://localhost:4000");
+
+const Task = () => {
+    return (
+        <div>
+            <Nav />
+            <AddTask socket={socket} />
+            <TasksContainer socket={socket} />
+        </div>
+    );
+};
+
+export default Task;
