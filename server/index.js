17799ac538010b66be9bc1cc64b5cd013f4ce7db
Weider Lima <weider.lima@mercadolivre.com>
2022-11-14 16:07:19 -0300

    finish initial config

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-//👇🏻index.js
 const express = require("express");
 const app = express();
 const PORT = 4000;
@@ -6,12 +5,32 @@ const PORT = 4000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//New imports
+const http = require("http").Server(app);
+const cors = require("cors");
+
+app.use(cors());
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+    socket.on('disconnect', () => {
+            socket.disconnect()
+      console.log('🔥: A user disconnected');
+    });
+});
+
 app.get("/api", (req, res) => {
     res.json({
         message: "Hello world",
     });
 });
 
-app.listen(PORT, () => {
+http.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
